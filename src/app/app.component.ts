da3552abebea5101a304e6394f81e21ca2d23777
Kadoya <monkenbai@gmail.com>
2019-08-06 15:48:45 +0900

    初回構築しました。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Ibook} from './interfaces/ibook';
+import data from './data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-list-table';
+export class AppComponent implements OnInit {
+  books: Ibook[] = data.books;
+
+  ngOnInit() {}
 }
